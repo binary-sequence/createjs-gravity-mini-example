d0ed3142cb864b7dcefdac61612e1fd2cd040148
Sergio <sergiolindo.empresa@gmail.com>
2013-08-27 09:32:44 +0200

    Test for properties on Ball()

--- tests/js/tests.js
+++ tests/js/tests.js
@@ -12,9 +12,11 @@ require(['js/libs/createjs-2013.05.14.min', 'js/Ball'], function(createjs, Ball)
 	test("Ball class", function() {
 		// CREATE INSTANCE OF BALL
 		result = new Ball();
-		equal( typeof(result), 'object', "typeof(result) == 'object'" );
+		equal( typeof(result), 'object', "typeof(Ball()) == 'object'" );
 		equal( result.x, 150, "Ball(), x = 150" );
 		equal( result.y, 50, "Ball() y = 50" );
+		equal( result.speed_x, 4, "Ball() speed_x = 4" );
+		equal( result.speed_y, 0, "Ball() speed_y = 0" );
 	});
 
 	console.info('End of tests.js');
