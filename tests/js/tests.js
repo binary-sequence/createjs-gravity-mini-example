ef8441b93562b038c93d3b0c642b3619d6470830
Sergio <sergiolindo.empresa@gmail.com>
2013-08-30 13:22:36 +0200

    Execute tests on document ready, add canvas in tests.html.

--- tests/js/tests.js
+++ tests/js/tests.js
@@ -6,64 +6,74 @@ require.config({
 	}
 });
 
-require(['js/libs/createjs-2013.05.14.min', 'js/classes/Ball', 'js/classes/GravityMiniExample'], function(createjs, Ball, GravityMiniExample) {
+require(['js/libs/requirejs.domReady', 'js/libs/createjs-2013.05.14.min', 'js/classes/Ball', 'js/classes/GravityMiniExample'], function(domReady, createjs, Ball, GravityMiniExample) {
 	console.info('Beginning of tests.js');
 
-	test("Ball class", function() {
-		var result = null;
-		var expected = null;
-		// Test properties on Ball().
-		result = new Ball();
-		equal( typeof(result), 'object', "typeof(Ball()) == 'object'" );
-		equal( result.x, 150, "Ball(), x = 150" );
-		equal( result.y, 50, "Ball() y = 50" );
-		equal( result.speed_x, 4, "Ball() speed_x = 4" );
-		equal( result.speed_y, 0, "Ball() speed_y = 0" );
-		// Test properties on Ball(1, 2, 3, 4).
-		result = new Ball(1, 2, 3, 4);
-		equal( typeof(result), 'object', "typeof(Ball(1, 2, 3, 4)) == 'object'" );
-		equal( result.x, 1, "Ball(1, 2, 3, 4), x = 1" );
-		equal( result.y, 2, "Ball(1, 2, 3, 4) y = 2" );
-		equal( result.speed_x, 3, "Ball(1, 2, 3, 4) speed_x = 3" );
-		equal( result.speed_y, 4, "Ball(1, 2, 3, 4) speed_y = 4" );
-		// Test update method.
-		result = new Ball(150, 50, 4, 0);
-		result.update();
-		expected = new Ball(150, 50, 4, 0);
-		expected.speed_y += 0.4;
-		expected.y += expected.speed_y;
-		expected.speed_x -= 0.01 * expected.speed_x;
-		expected.x += expected.speed_x;
-		deepEqual(result, expected, "update(), gravity, acceleration, friction.");
-		//
-		result = new Ball(150, 176, 4, 0);
-		result.update();
-		deepEqual(result.y, 176, "update(), bottom collision.");
-		//
-		result = new Ball(0, 50, -4, 0);
-		result.update();
-		expected = (-4 - 0.01 * -4) * -1;
-		deepEqual(result.speed_x, expected, "update(), left collision.");
-		//
-		result = new Ball(302, 50, 4, 0);
-		result.update();
-		expected = (4 - 0.01 * 4) * -1;
-		deepEqual(result.speed_x, expected, "update(), right collision.");
-		//
-		result = new Ball(150, 176, 4, 0.3);
-		result.update();
-		deepEqual(result.speed_y, 0, "update(), No infinite vertical movement.");
-		//
-		result = new Ball(150, 50, 0.01, 0);
-		result.update();
-		deepEqual(result.speed_x, 0, "update(), No infinite horizontal movement.");
-	});
+	domReady(function() {
+		test("Ball class", function() {
+			var result = null;
+			var expected = null;
+			// Test properties on Ball().
+			result = new Ball();
+			equal( typeof(result), 'object', "typeof(Ball()) == 'object'" );
+			equal( result.x, 150, "Ball(), x = 150" );
+			equal( result.y, 50, "Ball() y = 50" );
+			equal( result.speed_x, 4, "Ball() speed_x = 4" );
+			equal( result.speed_y, 0, "Ball() speed_y = 0" );
+			// Test properties on Ball(1, 2, 3, 4).
+			result = new Ball(1, 2, 3, 4);
+			equal( typeof(result), 'object', "typeof(Ball(1, 2, 3, 4)) == 'object'" );
+			equal( result.x, 1, "Ball(1, 2, 3, 4), x = 1" );
+			equal( result.y, 2, "Ball(1, 2, 3, 4) y = 2" );
+			equal( result.speed_x, 3, "Ball(1, 2, 3, 4) speed_x = 3" );
+			equal( result.speed_y, 4, "Ball(1, 2, 3, 4) speed_y = 4" );
+			// Test update method.
+			result = new Ball(150, 50, 4, 0);
+			result.update();
+			expected = new Ball(150, 50, 4, 0);
+			expected.speed_y += 0.4;
+			expected.y += expected.speed_y;
+			expected.speed_x -= 0.01 * expected.speed_x;
+			expected.x += expected.speed_x;
+			deepEqual(result, expected, "update(), gravity, acceleration, friction.");
+			//
+			result = new Ball(150, 176, 4, 0);
+			result.update();
+			deepEqual(result.y, 176, "update(), bottom collision.");
+			//
+			result = new Ball(0, 50, -4, 0);
+			result.update();
+			expected = (-4 - 0.01 * -4) * -1;
+			deepEqual(result.speed_x, expected, "update(), left collision.");
+			//
+			result = new Ball(302, 50, 4, 0);
+			result.update();
+			expected = (4 - 0.01 * 4) * -1;
+			deepEqual(result.speed_x, expected, "update(), right collision.");
+			//
+			result = new Ball(150, 176, 4, 0.3);
+			result.update();
+			deepEqual(result.speed_y, 0, "update(), No infinite vertical movement.");
+			//
+			result = new Ball(150, 50, 0.01, 0);
+			result.update();
+			deepEqual(result.speed_x, 0, "update(), No infinite horizontal movement.");
+		});
 
-	test("GravityMiniExample class", function() {
-		// Test properties on GravityMiniExample().
-		result = new GravityMiniExample();
-		deepEqual( typeof(result), 'object', "typeof(GravityMiniExample()) == 'object'" );
-		result.preload_data();
+		test("GravityMiniExample class", function() {
+			// Test properties on GravityMiniExample().
+			result = new GravityMiniExample();
+			deepEqual( typeof(result), 'object', "typeof(GravityMiniExample()) == 'object'" );
+			equal( result.debugMode, false, "GravityMiniExample(), debugMode == false" );
+			equal( result.width, 320, "GravityMiniExample(), width == 320" );
+			equal( result.height, 240, "GravityMiniExample(), height == 240" );
+			// Test properties on GravityMiniExample(true, 800, 600).
+			result = new GravityMiniExample(true, 800, 600);
+			deepEqual( typeof(result), 'object', "typeof(GravityMiniExample()) == 'object'" );
+			equal( result.debugMode, true, "GravityMiniExample(), debugMode == true" );
+			equal( result.width, 800, "GravityMiniExample(), width == 800" );
+			equal( result.height, 600, "GravityMiniExample(), height == 600" );
+		});
 	});
 
 	console.info('End of tests.js');
